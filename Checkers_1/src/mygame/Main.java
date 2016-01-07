56ece8a576119f54bd587a920af4a23d3823e13c
spike606 <krystusw@gmail.com>
2016-01-07 15:32:22 +0100

    podstawowe operacje

--- Checkers_1/src/mygame/Main.java
+++ Checkers_1/src/mygame/Main.java
@@ -9,18 +9,25 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.jme3.asset.plugins.ZipLocator;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 
 /**
  * Sample 3 - how to load an OBJ model, and OgreXML model, a material/texture,
  * or text.
  */
 public class Main extends SimpleApplication {
-
+        
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
+        
     }
-
+Spatial checker_czarny;
     @Override
     public void simpleInitApp() {
 
@@ -30,84 +37,92 @@ public class Main extends SimpleApplication {
 //        gameLevel.setLocalTranslation(0, -5.2f, 0);
 //        gameLevel.setLocalScale(2);
 //        rootNode.attachChild(gameLevel);
-        /**
-         * ***
-         */
-//        Spatial teapot = assetManager.loadModel("Models/Teapot/Teapot.obj");
-//        Material mat_default = new Material(
-//                assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
-//        teapot.setMaterial(mat_default);
-//        rootNode.attachChild(teapot);
-        // Create a wall with a simple texture from test_data
-//        Box box = new Box(2.5f, 2.5f, 1.0f);
-//        Spatial wall = new Geometry("Box", box);
-//        Material mat_brick = new Material(
-//                assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-//        mat_brick.setTexture("ColorMap",
-//                assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
-//        wall.setMaterial(mat_brick);
-//        wall.setLocalTranslation(2.0f, -2.5f, 0.0f);
-//        rootNode.attachChild(wall);
-        // Display a line of text with a default font1
-//        guiNode.detachAllChildren();
-//        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
-//        BitmapText helloText = new BitmapText(guiFont, false);
-//        helloText.setSize(guiFont.getCharSet().getRenderedSize());
-//        helloText.setText("Hello World");
-//        helloText.setLocalTranslation(300, helloText.getLineHeight(), 0);
-//        guiNode.attachChild(helloText);
-        // Load a model from test_data (OgreXML + material + texture)
-// Load a model from test_data (OgreXML + material + texture)
-//        Spatial ninja = assetManager.loadModel("Models/Sphere/Sphere.mesh.xml");
-//        ninja.scale(5f, 5f, 5f);
-//        ninja.rotate(0.0f, -3.0f, 0.0f);
-//        ninja.setLocalTranslation(10f, 5f, -2.0f);
-//        rootNode.attachChild(ninja);
-        
+       
         
         
         /* LOAD BOARD*/
-        Spatial board = assetManager.loadModel("Models/chessboard1/chessboard1.j3o");
+        Spatial board = assetManager.loadModel("Models/board/chessboard1.j3o");
         board.scale(1f, 1f, 1f);
-        board.rotate(0.0f, -3.0f, 0.0f);
+        board.rotate(0.0f, 0f, 0.0f);
         board.setLocalTranslation(0f, 0f, 0f);
         rootNode.attachChild(board);
         /**************/
         /* LOAD CHECKER*/
-        Spatial checker_bialy = assetManager.loadModel("Models/models_checkers/Checker_model_white.j3o");
-        checker_bialy.scale(0.5f, 0.5f, 0.5f);
-        checker_bialy.rotate(0.0f, -3.0f, 0.0f);
-        checker_bialy.setLocalTranslation(0.1f, 0.1f, 0.1f);
-        rootNode.attachChild(checker_bialy);
+//        Spatial checker_bialy = assetManager.loadModel("Models/checkers/Checker_model_white.j3o");
+//        checker_bialy.scale(0.5f, 0.5f, 0.5f);
+//        checker_bialy.rotate(0.0f, -3.0f, 0.0f);
+//        checker_bialy.setLocalTranslation(0f, 0f, 0f);
+//        rootNode.attachChild(checker_bialy);
         
-        Spatial checker_bialy_dama = assetManager.loadModel("Models/models_checkers/Checker_model_white_queen.j3o");
-        checker_bialy_dama.scale(0.5f, 0.5f, 0.5f);
-        checker_bialy_dama.rotate(0.0f, -3.0f, 0.0f);
-        checker_bialy_dama.setLocalTranslation(0.3f, 0.3f, 0.3f);
-        rootNode.attachChild(checker_bialy_dama);
+//        Spatial checker_bialy_dama = assetManager.loadModel("Models/checkers/Checker_model_white_queen.j3o");
+//        checker_bialy_dama.scale(0.5f, 0.5f, 0.5f);
+//        checker_bialy_dama.rotate(0.0f, -3.0f, 0.0f);
+//        checker_bialy_dama.setLocalTranslation(0.3f, 0.3f, 0.3f);
+//        rootNode.attachChild(checker_bialy_dama);
         
-        Spatial checker_czarny = assetManager.loadModel("Models/models_checkers/Checker_model_black.j3o");
-        checker_czarny.scale(0.5f, 0.5f, 0.5f);
-        checker_czarny.rotate(0.0f, -3.0f, 0.0f);
-        checker_czarny.setLocalTranslation(-10f, -10f, -10f);
+        checker_czarny = assetManager.loadModel("Models/checkers/Checker_model_black.j3o");
+        checker_czarny.scale(1f, 1f, 1f);
+        checker_czarny.rotate(0.0f, 0f, 0.0f);
+        checker_czarny.setLocalTranslation(0.2f, 0.63f, -0.2f);
         rootNode.attachChild(checker_czarny);
         
-        Spatial checker_czarny_dama = assetManager.loadModel("Models/models_checkers/Checker_model_black_queen.j3o");
-        checker_czarny_dama.scale(0.5f, 0.5f, 0.5f);
-        checker_czarny_dama.rotate(0.0f, -3.0f, 0.0f);
-        checker_czarny_dama.setLocalTranslation(20f, 20f, 20f);
-        rootNode.attachChild(checker_czarny_dama);
+        //prznies
+//        checker_czarny.move(2f, 2f, 2f);
+        
+        
+//        Spatial checker_czarny_dama = assetManager.loadModel("Models/checkers/Checker_model_black_queen.j3o");
+//        checker_czarny_dama.scale(0.5f, 0.5f, 0.5f);
+//        checker_czarny_dama.rotate(0.0f, -3.0f, 0.0f);
+//        checker_czarny_dama.setLocalTranslation(20f, 20f, 20f);
+//        rootNode.attachChild(checker_czarny_dama);
         /**************/
-        
-        
-        
-        
-        
+
         
         // You must add a directional light to make the model visible!
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f).normalizeLocal());
         rootNode.addLight(sun);
+        
+        
+        
+        initKeys(); // load my custom keybinding
 
     }
+    
+        protected Spatial thing;
+    /* Use the main event loop to trigger repeating actions. */
+    @Override
+    public void simpleUpdate(float tpf) {//time per second
+        // make the player rotate:
+//        checker_czarny.scale(0.2f, 0.2f, 0.2f);
+//        checker_czarny.scale(0.3f,0.3f,0.3f);
+//        checker_czarny.scale(0.2f,0.2f,0.2f);
+//        checker_czarny.scale(0.1f,0.1f,0.1f);
+
+    }
+    
+    
+      /** Custom Keybinding: Map named actions to inputs. */
+  private void initKeys() {
+    // You can map one or several inputs to one named action
+    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
+    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_K));
+    
+    // Add the names to the action listener.
+    inputManager.addListener(analogListener,"Left", "Right");
+  }
+    private AnalogListener analogListener = new AnalogListener() {
+    public void onAnalog(String name, float value, float tpf) {
+
+        if (name.equals("Right")) {
+          Vector3f v = checker_czarny.getLocalTranslation();
+          checker_czarny.setLocalTranslation(v.x + value*speed, v.y, v.z);
+        }
+        if (name.equals("Left")) {
+          Vector3f v = checker_czarny.getLocalTranslation();
+          checker_czarny.setLocalTranslation(v.x - value*speed, v.y, v.z);
+        }
+
+    }
+  };
 }
