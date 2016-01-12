ba3b56c61b1e67e025e713db3f20d3f328de46cf
spike606 <krystusw@gmail.com>
2016-01-12 12:43:44 +0100

    Dodanie kamer: 1,2,3,4,5

--- Checkers_1/src/mygame/Main.java
+++ Checkers_1/src/mygame/Main.java
@@ -16,8 +16,10 @@ import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.light.AmbientLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.scene.Node;
 import com.jme3.util.SkyFactory;
 
@@ -26,127 +28,183 @@ import com.jme3.util.SkyFactory;
  * or text.
  */
 public class Main extends SimpleApplication {
-        
+    
+    //przod
+    private static final Vector3f cam1Loc = new Vector3f(-6.2657156f, 14.529437f, 8.240483f);
+    private static final Vector3f cam1Dir = new Vector3f(-0.0028091485f, -0.7335623f, -0.67961645f);
+    private static final Vector3f cam1Up = new Vector3f(-0.003032215f, 0.67962223f, -0.733556f);
+    private static final Vector3f cam1Left = new Vector3f(-0.99999154f, -7.532071E-8f, -0.0041334783f);
+
+    //tyl
+    private static final Vector3f cam2Loc = new Vector3f(-6.150441f, 14.342193f, -20.968948f);
+    private static final Vector3f cam2Dir = new Vector3f(-0.004009098f, -0.7174297f, 0.6966194f);
+    private static final Vector3f cam2Up = new Vector3f(-0.0015367586f, 0.6966286f, 0.7174303f);
+    private static final Vector3f cam2Left = new Vector3f(0.99999076f, -0.0018057127f, 0.0038953684f);
+    
+    //z gory
+    private static final Vector3f cam3Loc = new Vector3f(-6.1873684f, 19.925018f, -3.8860335f);
+    private static final Vector3f cam3Dir = new Vector3f(0.0017965112f, -0.9943187f, -0.10642803f);
+    private static final Vector3f cam3Up = new Vector3f(-1.7530489E-4f, 0.10642797f, -0.9943204f);
+    private static final Vector3f cam3Left = new Vector3f(-0.99999833f, -0.0018049651f, -1.6890059E-5f);
+    
+    //z prawej
+    private static final Vector3f cam4Loc = new Vector3f(8.424002f, 14.017611f, -6.085185f);
+    private static final Vector3f cam4Dir = new Vector3f(-0.7035848f, -0.7105932f, -0.005055666f);
+    private static final Vector3f cam4Up = new Vector3f(-0.71059096f, 0.70360076f, -0.0025426447f);
+    private static final Vector3f cam4Left = new Vector3f(-0.005363941f, -0.0018036067f, 0.9999839f);
+    
+    // z rogu
+    private static final Vector3f cam5Loc = new Vector3f(-16.373577f, 19.19087f, -17.024492f);
+    private static final Vector3f cam5Dir = new Vector3f(0.37362808f, -0.82820004f, 0.4177161f);
+    private static final Vector3f cam5Up = new Vector3f(0.55454195f, 0.5604297f, 0.6151437f);
+    private static final Vector3f cam5Left = new Vector3f(0.7435626f, -0.0018061101f, -0.6686639f);
+    
+    
+    
     /*STALE DO POZYCJONOWANIA*/
     public static final float X_CELL = 1.809044164f;
     public static final float Z_CELL = 1.802715f;
 
-    
+    /*TABLICE DO BIEREK */
+    Spatial[] black_checkers = new Spatial[12];
+    Spatial[] white_checkers = new Spatial[12];
+
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
     }
-Spatial[] black_checkers = new Spatial[12];
-Spatial[] white_checkers = new Spatial[12];
-
 
     @Override
     public void simpleInitApp() {
 
-        /* adding scene*/
-//        assetManager.registerLocator("town.zip", ZipLocator.class);
-//        Spatial gameLevel = assetManager.loadModel("main.scene");
-//        gameLevel.setLocalTranslation(0, -5.2f, 0);
-//        gameLevel.setLocalScale(2);
-//        rootNode.attachChild(gameLevel);
-       
         
         
+        /* INFO OFF*/
+        setDisplayFps(false);
+        setDisplayStatView(false);
+        /*****/
+
         /* LOAD BOARD*/
         Spatial board = assetManager.loadModel("Models/board/chessboard2.j3o");
         board.scale(1f, 1f, 1f);
         board.rotate(0.0f, 0f, 0.0f);
         board.setLocalTranslation(0f, 0f, 0f);
         rootNode.attachChild(board);
-        /**************/
+        /**
+         * ***********
+         */
         /* LOAD CHECKER*/
 //        Spatial checker_bialy = assetManager.loadModel("Models/checkers/Checker_model_white.j3o");
 //        checker_bialy.scale(0.5f, 0.5f, 0.5f);
 //        checker_bialy.rotate(0.0f, -3.0f, 0.0f);
 //        checker_bialy.setLocalTranslation(0f, 0f, 0f);
 //        rootNode.attachChild(checker_bialy);
-        
-//        Spatial checker_bialy_dama = assetManager.loadModel("Models/checkers/Checker_model_white_queen.j3o");
-//        checker_bialy_dama.scale(0.5f, 0.5f, 0.5f);
-//        checker_bialy_dama.rotate(0.0f, -3.0f, 0.0f);
-//        checker_bialy_dama.setLocalTranslation(0.3f, 0.3f, 0.3f);
-//        rootNode.attachChild(checker_bialy_dama);
-        
-//        checker_czarny = assetManager.loadModel("Models/checkers/Checker_model_black.j3o");
-
-//        checker_czarny.scale(1f, 1f, 1f);
-//        checker_czarny.rotate(0.0f, 0f, 0.0f);
-//        checker_czarny.setLocalTranslation(0.042778164f, 1.4552078f, 0.0f);//POZYCJA 0!
-//      checker_czarny.setLocalTranslation(-1.766266f, 1.4552078f, 0.0f);//POZYCJA 1!
-//      checker_czarny.setLocalTranslation(-12.620530984f, 1.4552078f, 0.0f);//POZYCJA 7!
-
-        
         //przniesw
 //        checker_czarny.move(2f, 2f, 2f);
-        
-        
 //        Spatial checker_czarny_dama = assetManager.loadModel("Models/checkers/Checker_model_black_queen.j3o");
 //        checker_czarny_dama.scale(0.5f, 0.5f, 0.5f);
 //        checker_czarny_dama.rotate(0.0f, -3.0f, 0.0f);
 //        checker_czarny_dama.setLocalTranslation(20f, 20f, 20f);
 //        rootNode.attachChild(checker_czarny_dama);
-        /**************/
-        
-        
-            setUpCheckers();
-        
+        /**
+         * ***********
+         */
+        setUpCheckers();
+
+
         // You must add a directional light to make the model visible!
         DirectionalLight sun = new DirectionalLight();
         sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f).normalizeLocal());
         rootNode.addLight(sun);
-        
-        
+
+
+        cam.setFrame(cam1Loc, cam1Left, cam1Up, cam1Dir);
+        flyCam.setMoveSpeed(10);
+        //cam.lookAt(camLocVctrDir1,  Vector3f.UNIT_Y);
+
+
         //sky
         //viewPort.setBackgroundColor(ColorRGBA.Blue);       
 //        rootNode.attachChild(SkyFactory.createSky(
 //            assetManager, "Textures/sky/BrightSky.dds", false));
-                rootNode.attachChild(SkyFactory.createSky(
-            assetManager, "Textures/sky/space.dds", false));
+        rootNode.attachChild(SkyFactory.createSky(
+                assetManager, "Textures/sky/space.dds", false));
         initKeys(); // load my custom keybinding
 
     }
-    
-        protected Spatial thing;
+
     /* Use the main event loop to trigger repeating actions. */
     @Override
     public void simpleUpdate(float tpf) {//time per second
+        
+        System.out.println("Cam location: " + cam.getLocation());
+        System.out.println("Cam up : " + cam.getUp());
+        System.out.println("Cam left : " + cam.getLeft());
+        System.out.println("Cam direction : " + cam.getDirection());
+
+        
+        
         // make the player rotate:
 //        System.out.println(checker_czarny.getLocalTranslation());
 //        
 //        System.out.println("X: " + ((BoundingBox)checker_czarny.getWorldBound()).getXExtent()); 
 //        System.out.println("Y: " + ((BoundingBox)checker_czarny.getWorldBound()).getYExtent()); 
 //        System.out.println("Z: " + ((BoundingBox)checker_czarny.getWorldBound()).getZExtent()); 
-
-
-
-
 //        checker_czarny.scale(0.3f,0.3f,0.3f);
 //        checker_czarny.scale(0.2f,0.2f,0.2f);
 //        checker_czarny.scale(0.1f,0.1f,0.1f);
+    }
+
+    /**
+     * Custom Keybinding: Map named actions to inputs.
+     */
+    private void initKeys() {
+        // You can map one or several inputs to one named action
+        inputManager.addMapping("Cam1", new KeyTrigger(KeyInput.KEY_1));
+        inputManager.addMapping("Cam2", new KeyTrigger(KeyInput.KEY_2));
+        inputManager.addMapping("Cam3", new KeyTrigger(KeyInput.KEY_3));
+        inputManager.addMapping("Cam4", new KeyTrigger(KeyInput.KEY_4)); 
+        inputManager.addMapping("Cam5", new KeyTrigger(KeyInput.KEY_5));
+        
+        
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_J));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_K));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_I));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_M));
+        inputManager.addMapping("Far", new KeyTrigger(KeyInput.KEY_U));
+        inputManager.addMapping("Close", new KeyTrigger(KeyInput.KEY_O));
+        // Add the names to the action listener.
+        inputManager.addListener(analogListener, "Left", "Right", "Up", "Down", "Far", "Close");
+        inputManager.addListener(actionListener, "Cam1", "Cam2", "Cam3", "Cam4", "Cam5");
 
     }
     
-    
-      /** Custom Keybinding: Map named actions to inputs. */
-  private void initKeys() {
-    // You can map one or several inputs to one named action
-    inputManager.addMapping("Left",   new KeyTrigger(KeyInput.KEY_J));
-    inputManager.addMapping("Right",  new KeyTrigger(KeyInput.KEY_K));
-    inputManager.addMapping("Up",   new KeyTrigger(KeyInput.KEY_I));
-    inputManager.addMapping("Down",  new KeyTrigger(KeyInput.KEY_M));
-    inputManager.addMapping("Far",   new KeyTrigger(KeyInput.KEY_U));
-    inputManager.addMapping("Close",  new KeyTrigger(KeyInput.KEY_O));
-    // Add the names to the action listener.
-    inputManager.addListener(analogListener,"Left", "Right", "Up", "Down","Far", "Close");
-  }
-    private AnalogListener analogListener = new AnalogListener() {
-    public void onAnalog(String name, float value, float tpf) {
+    private ActionListener actionListener = new ActionListener() {
+    public void onAction(String name, boolean keyPressed, float tpf) {
+      if (name.equals("Cam1") && !keyPressed) {
+        cam.setFrame(cam1Loc, cam1Left, cam1Up, cam1Dir);
+      }
+      if (name.equals("Cam2") && !keyPressed) {
+        cam.setFrame(cam2Loc, cam2Left, cam2Up, cam2Dir);
+      }
+      if (name.equals("Cam3") && !keyPressed) {
+        cam.setFrame(cam3Loc, cam3Left, cam3Up, cam3Dir);
+      }
+      if (name.equals("Cam4") && !keyPressed) {
+        cam.setFrame(cam4Loc, cam4Left, cam4Up, cam4Dir);
+      }
+      if (name.equals("Cam5") && !keyPressed) {
+        cam.setFrame(cam5Loc, cam5Left, cam5Up, cam5Dir);
+      }
 
+    }
+  };
+    private AnalogListener analogListener = new AnalogListener() {
+        public void onAnalog(String name, float value, float tpf) {
+//        if (name.equals("Right")) {
+//          Vector3f v = checker_czarny.getLocalTranslation();
+//          checker_czarny.setLocalTranslation(v.x + value*speed, v.y , v.z);
+//        }
 //        if (name.equals("Right")) {
 //          Vector3f v = checker_czarny.getLocalTranslation();
 //          checker_czarny.setLocalTranslation(v.x + value*speed, v.y , v.z);
@@ -171,77 +229,69 @@ Spatial[] white_checkers = new Spatial[12];
 //          Vector3f v = checker_czarny.getLocalTranslation();
 //          checker_czarny.setLocalTranslation(v.x , v.y, v.z  - value*speed);
 //        }
-    }
-  };
-    
-    
-    private void setUpCheckers(){
-        
-        /** Create a pivot node at (0,0,0) and attach it to the root node */
+        }
+    };
+
+    private void setUpCheckers() {
+
+
         Node white_node = new Node("white_checkers");
         Node black_node = new Node("black_checkers");
 
         rootNode.attachChild(white_node); // put this node in the scene
         rootNode.attachChild(black_node); // put this node in the scene
+
         float cell_pos_x = 0.042778164f;
         float cell_pos_z = 0.0f;
 
-        for(int i = 0; i<12; i++){
+        for (int i = 0; i < 12; i++) {
             white_checkers[i] = assetManager.loadModel("Models/checkers/Checkers_model_white.j3o");
             black_checkers[i] = assetManager.loadModel("Models/checkers/Checkers_model_black.j3o");
 
-            if(i < 4){
-               cell_pos_x = 0.042778164f - i*X_CELL - X_CELL*(i+1);
-               cell_pos_z = 0.0f;
+            if (i < 4) {
+                cell_pos_x = 0.042778164f - i * X_CELL - X_CELL * (i + 1);
+                cell_pos_z = 0.0f;
             }
-            if(i>3 && i<8){
-               cell_pos_x = 0.042778164f - X_CELL*(i-4) - X_CELL*(i-4);
-               cell_pos_z = 0.0f - Z_CELL;
+            if (i > 3 && i < 8) {
+                cell_pos_x = 0.042778164f - X_CELL * (i - 4) - X_CELL * (i - 4);
+                cell_pos_z = 0.0f - Z_CELL;
             }
-            if(i>7){
-               cell_pos_x = 0.042778164f - (i-8)*X_CELL - X_CELL*(i-8+1);
-               cell_pos_z = 0.0f - Z_CELL *2;
+            if (i > 7) {
+                cell_pos_x = 0.042778164f - (i - 8) * X_CELL - X_CELL * (i - 8 + 1);
+                cell_pos_z = 0.0f - Z_CELL * 2;
             }
-            white_checkers[i].setLocalTranslation(cell_pos_x, 1.4552078f , cell_pos_z);
-            
-            
-            
-            
-            if(i < 4){
-               cell_pos_x = 0.042778164f - X_CELL*i - X_CELL*(i);
-               cell_pos_z = 0.0f - Z_CELL*7;
+            white_checkers[i].setLocalTranslation(cell_pos_x, 1.4552078f, cell_pos_z);
+
+            if (i < 4) {
+                cell_pos_x = 0.042778164f - X_CELL * i - X_CELL * (i);
+                cell_pos_z = 0.0f - Z_CELL * 7;
             }
-            if(i>3 && i<8){
+            if (i > 3 && i < 8) {
 //               cell_pos_x = 0.042778164f - i*X_CELL - X_CELL*(i-7);
-               cell_pos_x = 0.042778164f - (i-4)*X_CELL - X_CELL*(i-4+1);
+                cell_pos_x = 0.042778164f - (i - 4) * X_CELL - X_CELL * (i - 4 + 1);
 
-               cell_pos_z = 0.0f - Z_CELL*6;
+                cell_pos_z = 0.0f - Z_CELL * 6;
             }
-            if(i>7){
-               cell_pos_x = 0.042778164f - X_CELL*(i-8) - X_CELL*(i-8);
-               cell_pos_z = 0.0f - Z_CELL*5;
-            }      
-            
-            
-            
-            
-            black_checkers[i].setLocalTranslation(cell_pos_x, 1.4552078f , cell_pos_z);
+            if (i > 7) {
+                cell_pos_x = 0.042778164f - X_CELL * (i - 8) - X_CELL * (i - 8);
+                cell_pos_z = 0.0f - Z_CELL * 5;
+            }
 
-            //checker_czarny.setLocalTranslation(0.042778164f, 1.4552078f, 0.0f);//POZYCJA 0!
+            black_checkers[i].setLocalTranslation(cell_pos_x, 1.4552078f, cell_pos_z);
+
 
             white_node.attachChild(white_checkers[i]);
             black_node.attachChild(black_checkers[i]);
 
-//            black_node.attachChild(black_checkers[i]);
 
         }
-        
 
 
 
 
-     
 
-                
+
+
+
     }
 }
